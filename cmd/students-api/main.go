463c8a80e12ef6ad7ab90030416874fa7b1e172c
surajNirala <suraj.nirala1995@gmail.com>
2025-07-29 16:05:45 +0530

    server error fix

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/surajNirala/student-api/internal/config"
+)
 
 func main() {
-	fmt.Println("Welcome to the student api")
+	// Load Config
+	cfg := config.MustLoad()
+	// Database Setup
+	// Setup Router
+	router := http.NewServeMux()
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to students api"))
+	})
+	// Setup Server
+	server := http.Server{
+		Addr:    cfg.Addr,
+		Handler: router,
+	}
+
+	fmt.Printf("Server started %s", cfg.Addr)
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal("Failed to start server.")
+	}
 }
